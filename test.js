6df19cfd6939ef1a4d15dddf56ccda6c01de0372
Eugene Kravchuk <gene.int32@gmail.com>
2021-01-20 01:08:52 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,27 @@
 const test = require('ava');
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri =
+  "mongodb+srv://mongodb:27017";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 test('foo', t => {
 	t.pass();
